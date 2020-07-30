763b58d957f3868f8749e62fa0afc32381cc5cf6
nmaswood98 <nmaswood98@gmail.com>
2020-07-29 23:12:40 -0400

    Added group menu from the onepiratefree template from material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AppBar from "./components/AppBar";
+import ProjectList from "./components/ProjectList";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+
+import "./App.css";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#69696a",
+      main: "#28282a",
+      dark: "#1e1e1f",
+    },
+    secondary: {
+      light: "#fff5f8",
+      main: "#ff3366",
+      dark: "#e62958",
+    },
+  },
+  typography: {
+    fontFamily: "'Roboto Condensed', sans-serif",
+    fontSize: 14,
+    fontWeightLight: 300, // Work Sans
+    fontWeightRegular: 400, // Work Sans
+    fontWeightMedium: 700, // Roboto Condensed
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <ProjectList theme={theme} />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/AppBar.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { withStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import MuiAppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+const toolbarStyles = (theme: any) => ({
+  root: {
+    height: 64,
+    [theme.breakpoints.up("sm")]: {
+      height: 70,
+    },
+  },
+});
+
+const styles = (theme: any) => ({
+  title: {
+    fontSize: 24,
+    lineHeight: 1.6,
+  },
+  placeholder: toolbarStyles(theme).root,
+  toolbar: {
+    justifyContent: "space-between",
+  },
+  center: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+  },
+  leftLinkActive: {
+    color: theme.palette.common.white,
+  },
+  right: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  rightLink: {
+    fontSize: 16,
+    color: theme.palette.common.white,
+    marginLeft: theme.spacing(3),
+  },
+  linkSecondary: {
+    color: theme.palette.secondary.main,
+  },
+});
+
+function AppBar(props: any) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <MuiAppBar elevation={0} position="fixed">
+        <Toolbar className={classes.toolbar}>
+          <div className={classes.center}>
+            <Link
+              variant="h6"
+              underline="none"
+              color="inherit"
+              className={classes.title}
+            >
+              {"Nabhan Maswood"}
+            </Link>
+          </div>
+        </Toolbar>
+      </MuiAppBar>
+      <div className={classes.placeholder} />
+    </div>
+  );
+}
+
+AppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AppBar);
