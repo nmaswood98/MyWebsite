3ce8fee63509a3a1e08f9837583321d289a968be
nmaswood98 <nmaswood98@gmail.com>
2020-07-30 02:16:08 -0400

    Updates

---
+++ precache-manifest.80defef20ef3d620cf07f0ccab234ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99916285d044fe5a62e60639aeaf1a6d",
+    "url": "/MyWebsite/index.html"
+  },
+  {
+    "revision": "8229e8ccd5f3e71c58e4",
+    "url": "/MyWebsite/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "dae52ae35b54b55d1064",
+    "url": "/MyWebsite/static/js/2.5ea04f82.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MyWebsite/static/js/2.5ea04f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8229e8ccd5f3e71c58e4",
+    "url": "/MyWebsite/static/js/main.c9af3d2e.chunk.js"
+  },
+  {
+    "revision": "8db98dbc570c5d6a3d7c",
+    "url": "/MyWebsite/static/js/runtime-main.3c03dd98.js"
+  }
+]);
