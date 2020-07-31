df567fae1d3feb46ddf276b78e6a5972398ae306
nmaswood98 <nmaswood98@gmail.com>
2020-07-31 03:08:53 -0400

    Added badges for tech stack

--- src/components/TechBadges.jsx
+++ src/components/TechBadges.jsx
@@ -1,12 +1,41 @@
 import React from "react";
 import Badge from "./Badge/Badge";
 
+const techColorDictionary = {
+  Swift: "primary",
+  iOS: "info",
+  JavaScript: "success",
+  MapKit: "warning",
+  Firebase: "danger",
+  "Google Cloud Functions": "primary",
+  "Google Photos": "info",
+  React: "primary",
+  Redux: "info",
+  "Node.js": "success",
+  TypeScript: "warning",
+  "Pixi.js": "warning",
+  Primus: "danger",
+  Express: "info",
+  Unity2D: "danger",
+  "C#": "info",
+  AdMob: "success",
+  GameKit: "primary",
+  Android: "success",
+  "Objective-C": "warning",
+  SpriteKit: "danger",
+  "Cocoas-2D": "danger",
+};
+
 function TechBadges(props) {
   const types = ["primary", "info", "success", "warning", "danger"];
 
-  let randomType = () => {
-    let randomNumber = Math.floor(Math.random() * 4);
-    return types[randomNumber];
+  let getType = (tech) => {
+    if (techColorDictionary.hasOwnProperty(tech)) {
+      return techColorDictionary[tech];
+    } else {
+      let randomNumber = Math.floor(Math.random() * 4);
+      return types[randomNumber];
+    }
   };
 
   let techStringArray = props.technology.split(", ");
@@ -14,7 +43,7 @@ function TechBadges(props) {
   return (
     <div>
       {techStringArray.map((item) => (
-        <Badge color={randomType()}>{item}</Badge>
+        <Badge color={getType(item)}>{item}</Badge>
       ))}
     </div>
   );
